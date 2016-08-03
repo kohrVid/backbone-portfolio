1f22372cace176e47435a0fe663d8ebb515aac79
kohrVid <kohrVid@zoho.com>
2016-08-03 09:53:12 +0100

    Added boot.js and updated index page

---
+++ app/assets/javascripts/boot.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  var user = new app.models.User({
+    firstName: "Tim",
+    lastName: "Berners Lee",
+    imageUrl: "/uploads/tim.jpg",
+    bio: "I invented the internet (sort of)!",
+    mission: "Our mission is to make information freely available to all...I think"
+  });
+
+  view = new app.views.UserView({ model: user });
+  $("main#content").prepend(view.render().el);
+});
--- app/assets/javascripts/views/userView.js
+++ app/assets/javascripts/views/userView.js
@@ -2,14 +2,21 @@ app.views.UserView = Backbone.View.extend({
   tagName: 'section',
   id: 'bio',
   template: _.template($('#user-bio-template').html()),
+//  model: app.models.User,
 
   render: function () {
-    this.$el.html(this.template({
+    var scope = {
+      model: this.model
+    }
+    this.$el.html(this.template(scope));
+
+      /*
       imageUrl: this.model.get('imageUrl'),
       bio: this.model.get('bio'),
       mission: this.model.get('mission'),
       fullName: this.model.fullName()
-    })); 
+      model: this.model
+    })); */
     return this;
 
   }
